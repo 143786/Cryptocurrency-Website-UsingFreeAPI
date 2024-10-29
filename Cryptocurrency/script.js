59d2da277ad2da7dcf65dcd74de390432a093deb
Abdel Malik Tabish <91336061+143786@users.noreply.github.com>
2024-10-29 12:04:11 +0100

    Add files via upload

--- Cryptocurrency/script.js
+++ Cryptocurrency/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+let settings = {
+  async: true,
+  scrossDomain: true,
+  url: "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,DGC&tsyms=USD&api_key=5e680125c76bc23a2606b7f4891982481c39dc6dac5cc4ad33ddeb861dc106d1",
+  method: "GET",
+  headers: {},
+};
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
